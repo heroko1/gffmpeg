53365b09838788b8df36e57cb26b555b223a5488
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-13 16:37:42 +0530

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -254,7 +254,7 @@ async def encod(event):
         a2 = await info(out, e)
         dk = f"<b>File Name:</b> {newFile}\n\n<b>Original File Size:</b> {hbs(org)}\n<b>Encoded File Size:</b> {hbs(com)}\n<b>Encoded Percentage:</b> {per}\n\n<b>Get Mediainfo Here:</b> <a href='{a1}'>Before</a>/<a href='{a2}'>After</a>\n\n<i>Downloaded in {x}\nEncoded in {xx}\nUploaded in {xxx}</i>"
         ds = await e.client.send_file(
-            e.chat_id, file=ok, caption=bb, link_preview=False, thumb=thum, supports_streaming=True, name=bb
+            e.chat_id, file=ok, caption=bb, link_preview=False, thumb=output, supports_streaming=True, name=bb
         )
         os.remove(dl)
         os.remove(output)
