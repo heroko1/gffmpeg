5d068369be27a5c51acf6df9ddccfb4575a84d1a
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-14 19:30:18 +0530

    Add files via upload

---
+++ bot/__main__.py
@@ -0,0 +1,291 @@
+#    This file is part of the CompressorQueue distribution.
+#    Copyright (c) 2021 Danish_00
+#    Script Improved by Zylern
+
+
+from . import *
+from .config import *
+from .worker import *
+from .devtools import *
+from .FastTelethon import *
+import anitopy
+from telethon.tl.types import DocumentAttributeVideo
+LOGS.info("Starting...")
+
+try:
+    bot.start(bot_token=BOT_TOKEN)
+except Exception as er:
+    LOGS.info(er)
+
+
+####### GENERAL CMDS ########
+
+@bot.on(events.NewMessage(pattern="/start"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await start(e)
+
+
+@bot.on(events.NewMessage(pattern="/setcode"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await coding(e)
+
+
+@bot.on(events.NewMessage(pattern="/getcode"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await getcode(e)
+
+
+@bot.on(events.NewMessage(pattern="/showthumb"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await getthumb(e)
+
+
+@bot.on(events.NewMessage(pattern="/logs"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await getlogs(e)
+
+
+@bot.on(events.NewMessage(pattern="/cmds"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await zylern(e)
+
+
+@bot.on(events.NewMessage(pattern="/ping"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await up(e)
+
+
+@bot.on(events.NewMessage(pattern="/sysinfo"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await sysinfo(e)
+
+
+@bot.on(events.NewMessage(pattern="/leech"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await dl_link(e)
+
+
+@bot.on(events.NewMessage(pattern="/help"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await ihelp(e)
+
+
+@bot.on(events.NewMessage(pattern="/renew"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await renew(e)
+
+
+@bot.on(events.NewMessage(pattern="/clear"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await clearqueue(e)
+
+
+@bot.on(events.NewMessage(pattern="/speed"))
+async def _(e):
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
+        return e.reply("**Sorry You're not An Authorised User!**")
+    await test(e)
+    
+    
+
+########## Direct ###########
+
+@bot.on(events.NewMessage(pattern="/eval"))
+async def _(e):
+    await eval(e)
+
+@bot.on(events.NewMessage(pattern="/bash"))
+async def _(e):
+    await bash(e)
+
+
+######## Callbacks #########
+
+@bot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats(.*)")))
+async def _(e):
+    await stats(e)
+
+@bot.on(events.callbackquery.CallbackQuery(data=re.compile(b"skip(.*)")))
+async def _(e):
+    await skip(e)
+
+@bot.on(events.callbackquery.CallbackQuery(data=re.compile("help")))
+async def _(e):
+    await help(e)
+
+########## AUTO ###########
+
+@bot.on(events.NewMessage(incoming=True))
+async def _(event):
+        if str(event.sender_id) not in OWNER and event.sender_id !=DEV:
+            return await event.reply_text("**Sorry You're not An Authorised User!**")
+        if not event.photo:
+            return
+        os.system("rm thumb.jpg")
+        await event.client.download_media(event.media, file="/bot/thumb.jpg")
+        await event.reply("**Thumbnail Saved Successfully.**")
+
+
+@bot.on(events.NewMessage(incoming=True))
+async def _(e):
+    await asyncio.sleep(3)
+    await encod(e)
+
+
+async def something():
+    for i in itertools.count():
+        try:
+            if not WORKING and QUEUE:
+                user = int(OWNER.split()[0])
+                e = await bot.send_message(user, "**📥 Downloading Queue Files...**")
+                s = dt.now()
+                try:
+                    if isinstance(QUEUE[list(QUEUE.keys())[0]], str):
+                        dl = await fast_download(
+                            e, list(QUEUE.keys())[0], QUEUE[list(QUEUE.keys())[0]]
+                        )
+                    else:
+                        dl, file = QUEUE[list(QUEUE.keys())[0]]
+                        tt = time.time()
+                        dl = "downloads/" + dl
+                        with open(dl, "wb") as f:
+                            ok = await download_file(
+                                client=bot,
+                                location=file,
+                                out=f,
+                                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                                    progress(
+                                        d,
+                                        t,
+                                        e,
+                                        tt,
+                                        f"**📥 Downloading**\n__{dl.replace(f'downloads/', '')}__",
+                                    )
+                                ),
+                            )
+                except Exception as r:
+                    LOGS.info(r)
+                    WORKING.clear()
+                    QUEUE.pop(list(QUEUE.keys())[0])
+                es = dt.now()
+                kk = dl.split("/")[-1]
+                aa = kk.split(".")[-1]
+                newFile = dl.replace(f"downloads/", "").replace(f"_", " ")
+                rr = "encode"
+                bb = kk.replace(f".{aa}", ".mkv")
+                nam = bb
+                nam = nam.replace("_", " ")
+                nam = nam.replace(".mkv", " ")
+                nam = nam.replace(".mp4", " ")
+                nam = nam.replace(".", " ")
+                anitopy_options = {'allowed_delimiters': ' '}
+                new_name = anitopy.parse(nam)
+                anime_name = new_name['anime_title']  
+                joined_string = f"[{anime_name}]"
+                if 'anime_season' in new_name.keys():
+                  animes_season = new_name['anime_season']
+                  joined_string = f"{joined_string}" + f" [Season {animes_season}]"
+                if 'episode_number' in new_name.keys():
+                  episode_no = new_name['episode_number']
+                  joined_string = f"{joined_string}" + f" [Episode {episode_no}]"
+                og = joined_string + "[@ANIXPO]"                
+                out = f"{rr}/{bb}"
+                thum = "thumb1.jpg"
+                dtime = ts(int((es - s).seconds) * 1000)
+                hehe = f"{out};{dl};{list(QUEUE.keys())[0]}"
+                wah = code(hehe)
+                nn = await e.edit(
+                    "**🗜 Compressing...**",
+                    buttons=[
+                        [Button.inline("STATS", data=f"stats{wah}")],
+                        [Button.inline("CANCEL", data=f"skip{wah}")],
+                    ],
+                )
+                cmd = f"""ffmpeg -i "{dl}" {ffmpegcode[0]} "{out}" -y"""
+                duration, bitrate = await media_info(dl)
+                cmd1 = f'ffmpeg -i "{dl}" -map 0:v -ss 00:20 -frames:v 1 "{thum}" -y'
+                process = await asyncio.create_subprocess_shell(
+                    cmd1, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+                )
+                process = await asyncio.create_subprocess_shell(
+                    cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+                )
+                stdout, stderr = await process.communicate()
+                er = stderr.decode()
+                try:
+                    if er:
+                        await e.edit(str(er) + "\n\n**ERROR**")
+                        QUEUE.pop(list(QUEUE.keys())[0])
+                        os.remove(dl)
+                        os.remove(out)
+                        continue
+                except BaseException:
+                    pass
+                ees = dt.now()
+                ttt = time.time()
+                await nn.delete()
+                nnn = await e.client.send_message(e.chat_id, "**📤 Uploading...**")
+                with open(out, "rb") as f:
+                    ok = await upload_file(
+                        client=e.client,
+                        file=f,
+                        name=out,
+                        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                            progress(d, t, nnn, ttt, f"**📤 Uploading**\n__{out.replace(f'encode/', '')}__")
+                        ),
+                    )
+                await nnn.delete()
+                org = int(Path(dl).stat().st_size)
+                com = int(Path(out).stat().st_size)
+                pe = 100 - ((com / org) * 100)
+                per = str(f"{pe:.2f}") + "%"
+                eees = dt.now()
+                x = dtime
+                xx = ts(int((ees - es).seconds) * 1000)
+                xxx = ts(int((eees - ees).seconds) * 1000)
+                a1 = await info(dl, e)
+                a2 = await info(out, e)
+                dk = f"<b>File Name:</b> {newFile}\n\n<b>Original File Size:</b> {hbs(org)}\n<b>Encoded File Size:</b> {hbs(com)}\n<b>Encoded Percentage:</b> {per}\n\n<b>Get Mediainfo Here:</b> <a href='{a1}'>Before</a>/<a href='{a2}'>After</a>\n\n<i>Downloaded in {x}\nEncoded in {xx}\nUploaded in {xxx}</i>"
+                ds = await e.client.send_file(
+                    e.chat_id, file=ok, supports_streaming=True, caption=og, thumb=thum, attributes=[DocumentAttributeVideo(duration=duration, w=1280, h=720)]
+                )
+                QUEUE.pop(list(QUEUE.keys())[0])
+                os.remove(dl)
+                os.remove(out)
+                os.remove(thumb)
+            else:
+                await asyncio.sleep(3)
+        except Exception as err:
+            LOGS.info(err)
+
+
+########### Start ############
+
+LOGS.info("Bot has started.")
+with bot:
+    bot.loop.run_until_complete(something())
+    bot.loop.run_forever()
