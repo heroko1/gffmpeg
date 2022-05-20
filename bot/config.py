c153c1384fde03ed0fe764a629f19bfa6ae698dd
heroko1 <105900101+heroko1@users.noreply.github.com>
2022-05-20 19:41:35 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,8 +9,8 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1169977435
     OWNER = config("OWNER")
-    ffmpegcode = ["-i 'https://te.legra.ph/file/e9408e71281cdcb017874.png' -map 0 -filter_complex 'overlay =main_w-(overlay_w+10):main_h-(overlay_h+10)'  -c:v libx265 -crf 28 -c:s copy -s 854x480 -preset faster -metadata title='Visit For More Movies [t.me/AniXpo]'  -metadata:s:v title='Visit Website[Anixpo] t.me/AniXpo] - 480p - HEVC - 8bit'  -metadata:s:a title='[Visit t.me/AniXpo] - Opus - 60 kbps' -metadata:s:s title='[AniXpo Substations Alpha]' -c:a libopus -ab 60k"]
-    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
+    ffmpegcode = ["-map 0:v -map 0:a -b:a 192k -c:a aac -c:v libx265 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -crf 24.2 -map 0:s? -pix_fmt yuv420p -preset slow -profile:v main -vf smartblur=1.5:-0.35:-3.5:0.65:0.25:2.0,scale=1920:1080:spline16+accurate_rnd+full_chroma_int -x265-params me=2:rd=4:subme=7:aq-mode=3:aq-strength=1:deblock=1,1:psy-rd=1:psy-rdoq=1:rdoq-level=2:merange=57:bframes=8:b-adapt=2:limit-sao=1:frame-threads=3:no-info=1"]
+    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/18b4993d3f1e169479caf.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
